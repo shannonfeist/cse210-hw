9095fb6e2eb4d40a35df0637395bd4de3aca877a
shannonfeist <shannonfeist@yahoo.com>
2023-04-20 21:52:37 -0400

    new

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+              
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string guessinput = Console.ReadLine();
+            guess = int.Parse(guessinput);
+            
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
