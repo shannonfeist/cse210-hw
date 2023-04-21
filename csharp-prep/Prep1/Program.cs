84055a095e5d475e0dffd9ab1f15ba9ea6f06158
shannonfeist <shannonfeist@yahoo.com>
2023-04-20 20:25:32 -0400

    edited line 7 & 10

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name?");
         string first = Console.ReadLine();
 
-        Console.WriteLine("What is your last name?");
+        Console.Write("What is your last name?");
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
